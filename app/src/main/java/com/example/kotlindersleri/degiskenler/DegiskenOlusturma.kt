0d1998d2d9e75741f283a130f52864458e14f447
tubaranasakar <tubaranasakar@hotmail.com>
2024-08-22 23:13:07 +0300

    ilk commit

---
+++ app/src/main/java/com/example/kotlindersleri/degiskenler/DegiskenOlusturma.kt
@@ -0,0 +1,20 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    var ogrenciAdi = "Tuba"
+    var ogrenciYas = 23
+    var ogrenciBoy = 1.56
+    var ogrenciBasharf = 't'
+    var ogrenciDevamlilik = true
+
+    println("Öğrencinin adı : $ogrenciAdi")
+    println(ogrenciYas)
+    println(ogrenciBoy)
+    println(ogrenciBasharf)
+    println(ogrenciDevamlilik)
+
+    var a = 10
+    var b = 20
+    println("$a x $b = ${a*b}")
+
+}
---
+++ app/src/main/java/com/example/kotlindersleri/giris/Deneme.kt
@@ -0,0 +1,5 @@
+package com.example.kotlindersleri.giris
+
+fun main() {
+    println("Gene geldik en başa :) hehehe")
+}
